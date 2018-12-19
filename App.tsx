3e067dc375bace1f5e14c399b88c0599779f54b3
Minami Munakata <june28munazo@gmail.com>
2018-12-19 09:48:45 -0800

    create all navigators

--- App.tsx
+++ App.tsx
@@ -1,18 +1,42 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { LoginScreen } from "./Screens/LoginScreen";
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { LoginScreen } from './Screens/LoginScreen'
+import { ContactListScreen } from './Screens/ContactListScreen'
+import { AddContactScreen } from './Screens/AddContactScreen'
+import { SettingsScreen } from './Screens/SettingsScreen'
+import {
+  createStackNavigator,
+  createSwitchNavigator,
+  createBottomTabNavigator,
+  createAppContainer,
+} from 'react-navigation'
+
+const MainStack = createStackNavigator({
+  ContactList: ContactListScreen,
+  AddContact: AddContactScreen,
+})
+
+const MainTabs = createBottomTabNavigator({
+  Contacts: MainStack,
+  Settings: SettingsScreen,
+})
+
+const AppNavigator = createSwitchNavigator({
+  Login: LoginScreen,
+  Main: MainTabs,
+})
 
 export default class App extends React.Component {
   render() {
-    return <LoginScreen />;
+    return <LoginScreen />
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
